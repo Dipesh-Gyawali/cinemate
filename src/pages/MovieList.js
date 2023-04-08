2390ab7de744c22ba123088a94f4aeda22a6d511
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-04-08 06:40:09 +0545

    API fetch

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -1,15 +1,26 @@
 import { Card } from "../components";
+import { useState, useEffect} from "react";
 
 export const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchMovies(){
+      const response = await fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=817d754b375d2efa5619f570128e4b23");
+      const data = await response.json();
+      setMovies(data.results); //api ko results matra line vaneko
+    }
+    fetchMovies();
+  }, [])
+
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="flex  justify-start flex-wrap">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+        <div className="grid grid-cols-3 gap-4">
+          { movies.map((movie) => (
+            <Card key={movie.id} movie={movie} />
+
+          )) }
         </div> 
       </section>
     </main>
