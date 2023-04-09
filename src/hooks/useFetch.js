b5d3a10b681540bc4976ba2a1413c5cc95f17972
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-04-09 17:17:10 +0545

    search option

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,14 +1,14 @@
 import { useState, useEffect } from 'react'
 
-export const useFetch = (apiPath) => {
+export const useFetch = (apiPath, queryTerm="") => {
     const [data, setData] = useState([]);
-    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}`
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=${process.env.REACT_APP_API_KEY}&query=${queryTerm}`
 
     useEffect(() => {
         async function fetchMovies(){
           const response = await fetch(url);
           const json = await response.json();
-          setData(json.results); //api ko results matra line vaneko
+          setData(json.results); //api ko results vanne part matra line vaneko
         }
         fetchMovies();
       }, [url])
