7f0d2cf9a1e4338f4c63fba8fbf4836ae5b12559
Dipesh Gyawali <dipeshgyawali365@gmail.com>
2023-04-09 18:24:39 +0545

    responsiveness

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -8,7 +8,7 @@ export const MovieList = ({apiPath}) => {
   return (
     <main>
       <section className="max-w-7xl mx-auto py-7">
-        <div className="grid grid-cols-3 gap-4">
+        <div className="grid grid-cols-3 gap-4 other:grid-cols-2  others:grid-cols-1 others:mx-24">
           {movies.map((movie) => (
             <Card key={movie.id} movie={movie} />
           )) }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
-module.exports = {
+  module.exports = {
     content: ["./src/**/*.{html,js}"],
     darkMode: 'class',
     theme: {
-      extend: {},
+      extend: {
+        screens: {
+          "other": {'min': '700px', 'max': '1200px'},
+          "others": {'min': '0px', 'max': '700px'},
+        },
+        colors: {
+          darkbg: "#1E293B",
+          blue: {
+            850: "#1e40af"
+          }
+        }
+      },
     },
     plugins: [],
-  }
+}
